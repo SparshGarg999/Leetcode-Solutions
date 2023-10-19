6500af321faa0ed89c62e82b4bb214cc964d3912
Sparsh :) <76697238+SparshGarg999@users.noreply.github.com>
2023-10-19 15:46:29 +0530

    Time: 19 ms (20.61%), Space: 13.3 MB (27.59%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        def back(res, c):
+            if c != '#': res.append(c)
+            elif res: res.pop()
+            return res
+        return reduce(back, S, []) == reduce(back, T, [])
+        
